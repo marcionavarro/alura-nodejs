14d2325dd83b78df6488e72bda94d257ef93cf0e
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-03-25 00:19:29 -0300

    api-node-express [02-Avançando o tratamento de erros] - Refatorando o manipulador de erros,  Tratando página 404

--- api-node-express/src/app.js
+++ api-node-express/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 import manipulardorDeErros from "./midllewares/manipuladordeErros.js";
-
+import manipuladorde404 from "./midllewares/manipulador404.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 routes(app);
 
+app.use(manipuladorde404);
 
 app.use(manipulardorDeErros);
 
