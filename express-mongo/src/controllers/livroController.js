d298b19e1336aff26c569c61cf24deeea1ce402c
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-03-24 22:31:09 -0300

    express-mongo - Adicionando funcionalidades

--- express-mongo/src/controllers/livroController.js
+++ express-mongo/src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js"
+import { autor } from "../models/Autor.js"
 
 class LivroController {
     static async listarLivros(req, res) {
@@ -21,9 +22,13 @@ class LivroController {
     }
 
     static async cadastrarLivro(req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body)
-            res.status(201).json({ message: "Criado com sucesso", livro: novoLivro })
+            const autorEncontrado = await autor.findById(novoLivro.autor)
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } }
+            const livroCriado = await livro.create(livroCompleto)
+            res.status(201).json({ message: "Criado com sucesso", livro: livroCriado })
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` })
         }
@@ -33,7 +38,7 @@ class LivroController {
         try {
             const id = req.params.id
             await livro.findByIdAndUpdate(id, req.body)
-            res.status(200).json({message: "Livro atualizado"})
+            res.status(200).json({ message: "Livro atualizado" })
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha na atualização do livro` })
         }
@@ -43,9 +48,19 @@ class LivroController {
         try {
             const id = req.params.id
             await livro.findByIdAndRemove(id, req.body)
-            res.status(200).json({message: "Livro removido"})
+            res.status(200).json({ message: "Livro removido" })
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha na remoção do livro` })
+        }
+    }
+
+    static async listarLivrosPorEditor(req, res) {
+        const editora = req.query.editora
+        try {
+            const livroPorEditora = await livro.find({ editora: editora })
+            res.status(200).json(livroPorEditora)
+        } catch (error) {
+            res.status(500).json({ message: `${erro.message} - falha na busca da editora` })
         }
     }
 }
