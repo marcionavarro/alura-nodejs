7203b6faf8ae64ff8bbeacb8bc51b13a11f7923d
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-05 01:13:03 -0300

    nodejs-express-sequelize [03-Desenvolvendo a API] - Finalizando o CRUD

--- nodejs-express-sequelize/src/services/Services.js
+++ nodejs-express-sequelize/src/services/Services.js
@@ -8,6 +8,29 @@ class Services {
   async pegaTodosOsRegistros() {
     return dataSource[this.model].findAll();
   }
+
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro(dadosRegistro) {
+    return dataSource[this.model].create(dadosRegistro);
+  }
+
+  async atualizaRegistro(dadosAtualizados, id){
+    const listaDeRegistrosAtualizado = dataSource[this.model]
+      .update(dadosAtualizados, { where: { id : id}});
+
+    if(listaDeRegistrosAtualizado[0] === 0){
+      return false;
+    }
+
+    return true;
+  }
+
+  async excluirRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
 }
 
 module.exports = Services;
