17f5ea2f7f795cc9a64b0da94a57113a57048241
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-06 01:00:09 -0300

    nodejs-express-sequelize [01-Soft delete] - Revisando o projeto, Ocultar sem deletar

--- nodejs-express-sequelize/src/controllers/Controller.js
+++ nodejs-express-sequelize/src/controllers/Controller.js
@@ -2,57 +2,57 @@ class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
   }
-  
+
   async pegaTodos(req, res) {
     try {
       const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistro);
     } catch (erro) {
-      console.log(erro);
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
-  async pegaUmPorId(req, res){
+  async pegaUmPorId(req, res) {
     const { id } = req.params;
     try {
       const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
       return res.status(200).json(umRegistro);
     } catch (erro) {
-      console.log(erro);
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
-  async criaNovo(req, res){
+  async criaNovo(req, res) {
     const dadosParaCriacao = req.body;
     try {
       const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
       return res.status(200).json(novoRegistroCriado);
     } catch (erro) {
-      console.log(erro);
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
-  async atualiza(req, res){
+  async atualiza(req, res) {
     const { id } = req.params;
     const dadosAtualizados = req.body;
     try {
       const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
-      if(!foiAtualizado){
-        return res.status(400).json({mensagem: 'Registro não foi atualizado'});
-      }  
-      return res.status(200).json({mensagem: 'Atualizado com sucesso!'});
+      if (!foiAtualizado) {
+        return res.status(400).json({ mensagem: 'Registro não foi atualizado' });
+      }
+      return res.status(200).json({ mensagem: 'Atualizado com sucesso!' });
     } catch (erro) {
-      console.log(erro);
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
-  async exclui(req, res){
+  async exclui(req, res) {
     const { id } = req.params;
     try {
       await this.entidadeService.excluirRegistro(Number(id));
-      return res.status(200).json({mensagem: `id ${id} deletado com sucesso!`});
+      return res.status(200).json({ mensagem: `id ${id} deletado com sucesso!` });
     } catch (erro) {
-      console.log(erro);
+      return res.status(500).json({ erro: erro.message });
     }
   }
 }
