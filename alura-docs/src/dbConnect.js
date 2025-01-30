2f7aa4d7f861dd36ab39b3adcf55321a32c73549
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-01-30 01:34:22 -0300

    alura-docs [04-Utilizando o MongoDB] - Criando um banco de dados, Conectando o banco ao projeto, Obtendo dados do banco, Alterando dados do banco

---
+++ alura-docs/src/dbConnect.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const cliente = new MongoClient("mongodb+srv://marcionavarro:SUmd89PmCh6cIQV7@alura-docs.s2d5e.mongodb.net/");
+const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.s2d5e.mongodb.net/`
+const cliente = new MongoClient(url);
+
+let documentosColecao;
+
+try {
+    await cliente.connect();
+
+    const db = cliente.db("alura-websockets");
+    documentosColecao = db.collection("documentos");
+    console.log("🚀 ~ Conectado ao banco de dados com sucesso!")
+    
+} catch (erro) {
+    console.log("🚀 ~ error:", erro)
+}
+
+export { documentosColecao }
