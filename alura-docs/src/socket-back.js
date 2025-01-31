5a0503be0a9aa36de513236bbb048e54891d5aef
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-01-31 02:00:22 -0300

    alura-docs [05-Avançando o Alura Docs] - Listando os documentos do banco

--- alura-docs/src/socket-back.js
+++ alura-docs/src/socket-back.js
@@ -1,9 +1,11 @@
-import { atualizaDocumento, encontrarDocumento } from "./documentosDb.js";
+import { atualizaDocumento, encontrarDocumento, obterDocumentos } from "./documentosDb.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
-    console.log("Um cliente se conectou! ID:", socket.id);
-    console.log("MONGO_USER", process.env.MONGO_USER);
+    socket.on("obter_documentos", async (devolverDocumentos) => {
+        const documentos = await obterDocumentos();
+        devolverDocumentos(documentos)
+    })
 
     socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento);
